1774ac8346eef80a1c856e0a14bb18a5d9380f46
iamfrego <mate_freg@tiscali.it>
2021-10-13 15:14:02 +0200

    Esercizio Fizz Buzz

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+// alert("Connesso");  //check connessione
+
+const ul = document.querySelector("ul.list");
+
+for (let i = 1; i <= 100; i++) {
+
+    if (i % 15 === 0) {
+        const element = `<li class="box box--${i} fizzbuzz">FizzBuzz</li>`
+        ul.innerHTML += element;
+    } else if (i % 5 === 0) {
+        const element = `<li class="box box--${i} buzz">buzz</li>`
+        ul.innerHTML += element;
+    } else if (i % 3 === 0) {
+        const element = `<li class="box box--${i} fizz">Fizz</li>`
+        ul.innerHTML += element;
+    } else {
+        const element = `<li class="box box--${i} number">${i}</li>`
+        ul.innerHTML += element;
+    }
+
+}
