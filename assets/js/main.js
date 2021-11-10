1de2afbadd5405920f7840d9d74e0f7a9c1b60e4
iamfrego <mate_freg@tiscali.it>
2021-11-10 10:13:15 +0100

    New Pc Sync

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,16 +5,16 @@ const ul = document.querySelector("ul.list");
 for (let i = 1; i <= 100; i++) {
 
     if (i % 15 === 0) {
-        const element = `<li class="box box--${i} fizzbuzz">FizzBuzz</li>`
+        const element = `<li class="box box--${i} fizzbuzz">FizzBuzz</li>`;
         ul.innerHTML += element;
     } else if (i % 5 === 0) {
-        const element = `<li class="box box--${i} buzz">buzz</li>`
+        const element = `<li class="box box--${i} buzz">buzz</li>`;
         ul.innerHTML += element;
     } else if (i % 3 === 0) {
-        const element = `<li class="box box--${i} fizz">Fizz</li>`
+        const element = `<li class="box box--${i} fizz">Fizz</li>`;
         ul.innerHTML += element;
     } else {
-        const element = `<li class="box box--${i} number">${i}</li>`
+        const element = `<li class="box box--${i} number">${i}</li>`;
         ul.innerHTML += element;
     }
 }
